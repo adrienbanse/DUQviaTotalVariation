73572a8a26d92a0cf2984ed6f32c179d99bf12a2
Eduardo Figueiredo Mota Diniz Costa <efigueiredomot@tudelft.nl>
2024-02-13 16:37:42 +0100

    [Organize] Keep reorganizing methods

--- bounds_linear_system_2d.py
+++ bounds_linear_system_2d.py
@@ -1,4 +1,6 @@
 import propagation_methods as propag
+import bounds_common as common
+import grid_generation as grid
 
 import numpy as np
 from scipy import special
@@ -7,162 +9,29 @@ from scipy import special
 # ------------------------------ Auxiliary computing methods ------------------------------ #
 # ----------------------------------------------------------------------------------------- #
 
-Pi = np.pi
-
-def exp(x):
-    return np.exp(x)
-
 def sqrt(x):
     return np.sqrt(x)
 
 def erf(x):
     return special.erf(x)
 
-def erfc(x):
-    return special.erfc(x)
-
-def log(x):
-    return np.log(x)
-
 
 # ----------------------------------------------------------------------------------------- #
 # ----------------------------- Auxiliary maximization methods ---------------------------- #
 # ----------------------------------------------------------------------------------------- #
 
-def KLTerm(A, x, signature, cov_noise, i):
-    return 1/cov_noise[i][i]*(np.matmul(A, x)[i] - np.matmul(A, signature)[i])**2
-
-def getVertices(region):
-    
-    vertice1 = [region[0][0], region[1][0]]
-    vertice2 = [region[0][0], region[1][1]]
-    vertice3 = [region[0][1], region[1][0]]
-    vertice4 = [region[0][1], region[1][1]]
-
-    return [vertice1, vertice2, vertice3, vertice4]
-
-def verifyIfRegionIsBounded(region):
-    if np.isinf(region[0][0]) or np.isinf(region[0][1]) or np.isinf(region[1][0]) or np.isinf(region[1][1]):
-        return False
-    return True
+def insideSumTerm(A, x, signature, cov_noise, i):
+    return 1/(cov_noise[i][i]**0.5)*abs(np.matmul(A, x)[i] - np.matmul(A, signature)[i])
 
 
 def maxValueInsideRegion(A, signature, region, cov_noise):
 
     max_value_0, max_value_1 = 0, 0
 
-    vertices = getVertices(region)
-
-    #print(signature)
-    #print(vertices)
+    vertices = grid.getVertices(region)
     
     for vertice in vertices:
-        max_value_0 = max(max_value_0, KLTerm(A, vertice, signature, cov_noise, 0))
-        max_value_1 = max(max_value_1, KLTerm(A, vertice, signature, cov_noise, 1))
+        max_value_0 = max(max_value_0, insideSumTerm(A, vertice, signature, cov_noise, 0))
+        max_value_1 = max(max_value_1, insideSumTerm(A, vertice, signature, cov_noise, 1))
 
     return max_value_0 + max_value_1
-
-
-# ----------------------------------------------------------------------------------------- #
-# -------------------------- Measure of Sqrt[KL divergence] - ALTERNATIVE ------------------------------- #
-# ----------------------------------------------------------------------------------------- #
-
-def computeTV_withMax(A, signatures, regions, gmm, cov_noise):
-
-    #prob_check = 0
-
-    tv = 0
-    for signature, region in zip(signatures, regions):
-        
-        max_value = 0
-        prob_gmm_region = 0
-
-        if verifyIfRegionIsBounded(region):
-            max_value = erf(maxValueInsideRegion(A, signature, region, cov_noise)**0.5)/(2*np.sqrt(2))
-        else:
-            max_value = 1
-
-        
-        #print(max_value)
-        for (weight, gmm_mean) in zip(gmm[0], gmm[1]):
-            prob_gmm_region += weight*propag.gaussianProbaMassInsideHypercube(gmm_mean, gmm[2], region)
-
-        #prob_check += prob_gmm_region
-
-        tv += max_value*prob_gmm_region
-
-
-    #print(prob_check)
-
-    return tv
-
-
-
-
-
-
-# ----------------------------------------------------------------------------------------- #
-# -------------------------- Measure of Sqrt[KL divergence] ------------------------------- #
-# ----------------------------------------------------------------------------------------- #
-
-def computeFoldedMomentum(sigCompon, meanX, covX, region):
-
-    factor_constant = 0.5*sqrt(Pi/2)*sqrt(covX[0][0]*covX[1][1])
-
-    factor_exponential = 2*sqrt(covX[0][0])*(np.exp(-0.5*(region[0][1] - meanX[0])**2/covX[0][0]) - 2*np.exp(-0.5*(sigCompon - meanX[0])**2/covX[0][0]) + np.exp(-0.5*(meanX[0] - region[0][0])**2/covX[0][0]))
-
-    factor_erf = sqrt(2*Pi)*(sigCompon - meanX[0])*(erf((region[0][1] - meanX[0])/(sqrt(2*covX[0][0]))) + erf((region[0][0] - meanX[0])/(sqrt(2*covX[0][0]))) - 2*erf((sigCompon - meanX[0])/(sqrt(2*covX[0][0]))))
-
-    factor_x2 = erf((region[1][0] - meanX[1])/(sqrt(2*covX[1][1]))) + erf((meanX[1] - region[1][1])/(sqrt(2*covX[1][1])))
-    
-    return factor_constant*(factor_exponential + factor_erf)*factor_x2
-
-
-def computeMeasureSqrtKLWrtGMMComponent(A, cov_noise, signature, meanX, covX, region):
-
-    factor_constant_x1 = abs(A[0][0])/sqrt(cov_noise[0][0]) + abs(A[1][0])/sqrt(cov_noise[1][1])
-    factor_integral_x1 = computeFoldedMomentum(signature[0], meanX, covX, region)
-
-    #Use symmetry
-    inverted_meanX = [meanX[1], meanX[0]]
-    inverted_covX = [[covX[1][1], 0.0],
-                     [0.0, covX[0][0]]]
-    inverted_region = [region[1], region[0]]
-    
-    factor_constant_x2 = abs(A[0][1])/sqrt(cov_noise[0][0]) + abs(A[1][1])/sqrt(cov_noise[1][1])
-    factor_integral_x2 = computeFoldedMomentum(signature[1], inverted_meanX, inverted_covX, inverted_region)
-
-    result = factor_constant_x1*factor_integral_x1 + factor_constant_x2*factor_integral_x2
-
-    return result/sqrt(2)
-
-
-def computeMeasureSqrtKLWrtGMM(A, signature, region, cov_noise, gmm):
-
-    measure = 0
-
-    weights = gmm[0]
-    gmm_means = gmm[1]
-    gmm_cov = gmm[2]
-
-    for (weight, mean_x) in zip(weights, gmm_means):
-
-        measure += weight*computeMeasureSqrtKLWrtGMMComponent(A, cov_noise, signature, mean_x, gmm_cov, region)
-    
-    return measure
-
-
-# ----------------------------------------------------------------------------------------- #
-# --------------------------------- Total Variation bound --------------------------------- #
-# ----------------------------------------------------------------------------------------- #
-
-def computeTVbound(A, signatures, regions, cov_noise, gmm):
-
-    tv_bound = 0
-
-    for k, region in enumerate(regions):
-    
-        sqrtKL_term_region = computeMeasureSqrtKLWrtGMM(A, signatures[k], region, cov_noise, gmm)
-        tv_bound += sqrtKL_term_region
-
-    return sqrt(2)*tv_bound
--- propagation_methods.py
+++ propagation_methods.py
@@ -1,9 +1,9 @@
+import grid_generation as grid
+import probability_mass_computation as proba
+
 import numpy as np
 import math
-import matplotlib.pyplot as plt
-from scipy.stats import norm
 from scipy.stats import multivariate_normal
-from scipy import special
 
 
 def getInitialState(mean, cov):
@@ -14,11 +14,6 @@ def getInitialState(mean, cov):
     return np.array(initial_state_samples)
 
 
-def systemDynamics(A, x):
-
-    return np.matmul(A, x)
-
-
 def systemNoise(mean, cov):
     
     multivariate_gaussian = multivariate_normal(mean = mean, cov = cov)
@@ -27,7 +22,25 @@ def systemNoise(mean, cov):
     return np.array(noise_samples)
 
 
-def stateEvolution(A, initial_state, mean_noise, cov_noise, n_steps_ahead):
+def systemDynamics(x, method, params):
+
+    if method == 'linear':
+        A = params[0]
+        return np.matmul(A, x)
+    
+    elif method == 'polynomial':
+        h = params[0]
+
+        component_1 = x[0] + h*x[1]
+        component_2 = x[1] + h*(1/3*x[0]**3 - x[0] - x[1])
+
+        return np.array([component_1, component_2])
+    
+    else:
+        raise Exception("Please refer to the method's signature")
+
+
+def stateEvolution(initial_state, mean_noise, cov_noise, n_steps_ahead, method, params):
     
     state = initial_state
 
@@ -35,19 +48,19 @@ def stateEvolution(A, initial_state, mean_noise, cov_noise, n_steps_ahead):
 
         noise = systemNoise(mean_noise, cov_noise)
         
-        state = systemDynamics(A, state) + noise
+        state = systemDynamics(state, method, params) + noise
     
     return state
 
 
-def monteCarloSimulationSystem(n_simulations, A, mean_initial_state, cov_initial_state, mean_noise, cov_noise, n_steps_ahead):
+def monteCarloSimulationSystem(n_simulations, mean_initial_state, cov_initial_state, mean_noise, cov_noise, n_steps_ahead, method, params):
     
     initial_states, final_states = [], []
     
     for i in range(n_simulations):
     
         initial_state = getInitialState(mean_initial_state, cov_initial_state)
-        final_state = stateEvolution(A, initial_state, mean_noise, cov_noise, n_steps_ahead)
+        final_state = stateEvolution(initial_state, mean_noise, cov_noise, n_steps_ahead, method, params)
 
         initial_states.append(initial_state)
         final_states.append(final_state)
@@ -55,139 +68,7 @@ def monteCarloSimulationSystem(n_simulations, A, mean_initial_state, cov_initial
     return initial_states, final_states
 
 
-def erf_factor(mean, variance, region_min, region_max):
-    return special.erf((mean - region_min)/(np.sqrt(2*variance))) - special.erf((mean - region_max)/(np.sqrt(2*variance)))
-
-
-def gaussianProbaMassInsideHypercube(mean, cov, cube):
-    #Assuming cube = [[x_min, x_max], [y_min, y_max]]
-
-    factor = 1/4
-    x_erf = erf_factor(mean[0], cov[0][0], cube[0][0], cube[0][1])
-    y_erf = erf_factor(mean[1], cov[1][1], cube[1][0], cube[1][1])
-
-    return factor*x_erf*y_erf
-
-
-def generateIntervalsPerAxis(bounds, n_partitions):
-
-    center = bounds[1] - bounds[0]
-    sigma = center/6
-
-    factor = 8
-
-    lim0 = bounds[0]
-    lim1 = bounds[0] + sigma
-    lim2 = bounds[0] + 2*sigma
-    lim3 = bounds[0] + 3*sigma
-    lim4 = bounds[0] + 4*sigma
-    lim5 = bounds[0] + 5*sigma
-    lim6 = bounds[1]
-
-    factor_high = 0.68
-    factor_medium = 0.27/2
-
-    n_partitions_center = math.floor(factor_high*n_partitions)
-    n_partitions_medium_left = math.floor(factor_medium*n_partitions)
-    n_partitions_medium_right = math.floor(factor_medium*n_partitions)
-
-    partial_sum = n_partitions_center + n_partitions_medium_left + n_partitions_medium_right
-    remaining_partitions = n_partitions - partial_sum
-
-    if remaining_partitions <= 0:
-        n_partitions_low_left, n_partitions_low_right = 0, 0
-    else:
-        if remaining_partitions % 2 == 0:
-            n_partitions_low_left = int(remaining_partitions/2)
-            n_partitions_low_right = int(remaining_partitions/2)
-        else:
-            n_partitions_center += 1
-            remaining_partitions -= 1
-            n_partitions_low_left = int(remaining_partitions/2)
-            n_partitions_low_right = int(remaining_partitions/2)
-
-    points = np.linspace(lim0, lim1, n_partitions_low_left)
-    points = np.append(points, np.linspace(lim1, lim2, n_partitions_medium_left))
-    points = np.append(points, np.linspace(lim2, lim4, n_partitions_center))
-    points = np.append(points, np.linspace(lim4, lim5, n_partitions_medium_right))
-    points = np.append(points, np.linspace(lim5, lim6, n_partitions_low_right))
-
-    #points = np.linspace(bounds[0], bounds[1], n_partitions)
-    points = np.append(points, -np.inf)
-    points = np.append(points, np.inf)
-    points = np.sort(points)
-
-    return points
-
-
-def createRegions(bounds_x, bounds_y, n_partitions):
-
-    x_points = generateIntervalsPerAxis(bounds_x, n_partitions)
-    y_points = generateIntervalsPerAxis(bounds_y, n_partitions)
-
-    regions = []
-
-    for i, x in enumerate(x_points):
-        for j, y in enumerate(y_points):
-                
-            if i > 0 and j > 0:
-                
-                region = [[x_points[i-1], x_points[i]],
-                        [y_points[j-1], y_points[j]]]
-            
-                regions.append(region)
-
-    return regions
-
-
-def computePosition(min, max, delta):
-
-    if np.isinf(min):
-            position = max - delta
-    elif np.isinf(max):
-            position = min + delta
-    else:
-            position = (min + max)/2
-
-    return position
-
-
-def placeSignatures(regions, delta):
-
-    signatures = []
-
-    for region in regions:
-        
-        signature_point = [
-                            computePosition(region[0][0], region[0][1], delta), 
-                            computePosition(region[1][0], region[1][1], delta)
-                          ]
-        
-        signatures.append(signature_point)
-    
-    return signatures
-
-
-def computeSignatureProbabilities(regions, means, cov, weights):
-    #Assumes a GMM where the components are described by means and covs(initial step becomes a GMM w/ 1 component)
-    #RECALL THAT THE COVARIANCE IS THE SAME FOR ALL CONDITIONAL DIST IN THIS CASE
-    #This can be more generic for other cases 
-    
-    signature_probas = []
-
-    for region in regions:
-        
-        proba_region = 0
-        
-        for (mean, weight) in zip(means, weights):
-            proba_region += weight*gaussianProbaMassInsideHypercube(mean, cov, region)
-        
-        signature_probas.append(proba_region)
-    
-    return signature_probas
-
-
-def propagateSignatures(A, signatures):
+def propagateSignatures(signatures, method, params):
     
     #Returns the propagation of the signature points (this is useful because we are fixating the signatures)
 
@@ -195,17 +76,17 @@ def propagateSignatures(A, signatures):
 
     for point in signatures:
         
-        propagated_point = systemDynamics(A, point)
+        propagated_point = systemDynamics(point, method, params)
         gaussian_means.append(propagated_point)
     
     return gaussian_means
 
 
-def sampleFromGMM(A, n_samples, weights, signatures, cov):
+def sampleFromGMM(n_samples, weights, signatures, cov, method, params):
                 
     sampled_points = []
 
-    means_gmm = propagateSignatures(A, signatures) #See equation to see the need of this propagation (THINK: IT SEEMS TO DEPEND ON HOW WE BUILD PROPAGATION)
+    means_gmm = propagateSignatures(signatures, method, params) #See equation to see the need of this propagation (THINK: IT SEEMS TO DEPEND ON HOW WE BUILD PROPAGATION)
     #means_gmm = signatures
 
     for i in range(n_samples):
@@ -235,16 +116,16 @@ def defineRegionParameters(samples):
     return [mean_x0, stdev_x0], [mean_x1, stdev_x1]
 
 
-def propagateUncertaintyOneStep(A, regions, signatures, probas, cov_noise):
+def propagateUncertaintyOneStep(regions, signatures, probas, cov_noise, method, params):
 
     #Propagate signature points (signatures are fixed)
     #Later we can think about moving the signature points each time, so this will have to be adapted
     #propagated_signatures = propagateSignatures(signatures)
 
     #This has to come before the definition of the new signatures!!!! Nice :)
-    means_gmm = propagateSignatures(A, signatures) #OR SHOULD IT BE JUST SIGNATURES?
+    means_gmm = propagateSignatures(signatures, method, params) #OR SHOULD IT BE JUST SIGNATURES?
 
-    samples = sampleFromGMM(A, 1000, probas, signatures, cov_noise)
+    samples = sampleFromGMM(1000, probas, signatures, cov_noise, method, params)
         
     params_x0, params_x1 = defineRegionParameters(samples)
 
@@ -253,12 +134,12 @@ def propagateUncertaintyOneStep(A, regions, signatures, probas, cov_noise):
     print(n_signatures)
 
     #regions = createRegions([params_x0[0] - 3*params_x0[1], params_x0[0] + 3*params_x0[1]], [params_x1[0] - 3*params_x1[1], params_x1[0] + 3*params_x1[1]], math.floor(math.sqrt(len(signatures)))-1)
-    regions = createRegions([params_x0[0] - 4*params_x0[1], params_x0[0] + 4*params_x0[1]], [params_x1[0] - 4*params_x1[1], params_x1[0] + 4*params_x1[1]], n_signatures)
-    signatures = placeSignatures(regions, 0.5)
+    regions = grid.createRegions([params_x0[0] - 4*params_x0[1], params_x0[0] + 4*params_x0[1]], [params_x1[0] - 4*params_x1[1], params_x1[0] + 4*params_x1[1]], n_signatures)
+    signatures = grid.placeSignatures(regions, 0.5)
 
     #means_gmm = signatures
 
     #means_gmm = propagateMeans(means_gmm)
-    probas = computeSignatureProbabilities(regions, means_gmm, cov_noise, probas) 
+    probas = proba.computeSignatureProbabilities(regions, means_gmm, cov_noise, probas) 
 
     return probas, signatures, regions
